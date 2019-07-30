47303741abbd7cd873ed75b3625e0868c1880ada
kalpana1405 <30996110+kalpana1405@users.noreply.github.com>
2019-07-30 14:28:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
   public static void main (String args[]){
-  System.out.println("Hello world from hello-world branch");
-    System.out.println("First cahnge i have made to it");
+  System.out.println("Hello world from hello-world master");
+   
   }
 }
